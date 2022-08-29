9291a55b90b2c887bab06fcc71488caad16192db
Roy <iRoySwift@iCloud.com>
2022-08-29 16:36:16 +0800

    ✨ websocket node

--- app/xterm/src/index.ts
+++ app/xterm/src/index.ts
@@ -1,3 +1,13 @@
+// import { base64Decode } from './utils';
+const utils = require('./utils');
+
+/*
+ * @Author: Roy
+ * @Date: 2022-06-16 09:54:17
+ * @LastEditors: Roy
+ * @LastEditTime: 2022-08-29 14:46:32
+ * @Description: websocket
+ */
 const express = require('express');
 const expressWs = require('express-ws');
 const app = express();
@@ -7,7 +17,7 @@ const pty = require('node-pty');
 const os = require('os');
 const shell = os.platform() === 'win32' ? 'powershell.exe' : 'bash';
 
-app.all('*', function (req, res, next) {
+app.all('*', function (_, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   res.header('Access-Control-Allow-Methods', '*');
@@ -20,7 +30,7 @@ function nodeEnvBind() {
   const term = pty.spawn(shell, ['--login'], {
     name: 'xterm-color',
     cols: 80,
-    rows: 24,
+    rows: 240,
     cwd: process.env.HOME,
     env: process.env
   });
@@ -28,8 +38,8 @@ function nodeEnvBind() {
   return term;
 }
 //服务端初始化
-app.post('/terminal', (req, res) => {
-  const term = nodeEnvBind(req);
+app.post('/terminal', (_, res) => {
+  const term = nodeEnvBind();
   res.send(term.pid.toString());
   res.end();
 });
@@ -37,10 +47,23 @@ app.ws('/socket/:pid', (ws, req) => {
   const pid = parseInt(req.params.pid);
   const term = termMap.get(pid);
   term.on('data', function (data) {
-    ws.send(data);
+    console.log('1', data, '--data--');
+    const message = `1${utils.base64Encode(data)}`;
+
+    ws.send(message);
   });
   ws.on('message', (data) => {
-    term.write(data);
+    const type = data.substr(0, 1);
+    const msg = utils.base64Decode(data.substr(1));
+
+    console.log('2', data, msg, '--message--');
+    if (`${type}` === '4') {
+      const obj = JSON.parse(msg);
+      term.resize(obj.Width, obj.Height);
+    }
+    if (`${type}` === '0') {
+      term.write(msg);
+    }
   });
   ws.on('close', function () {
     term.kill();
