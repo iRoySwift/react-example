3207722be11055ab55b1b16daaee12355bd1632e
Roy <iRoySwift@iCloud.com>
2023-04-04 16:12:23 +0800

    graph

--- packages/graphin/src/graph/index.ts
+++ packages/graphin/src/graph/index.ts
@@ -1,7 +1,16 @@
-import G6, { GraphOptions } from '@antv/g6';
-import Util from '../util/index';
+/*
+ * @Author: Roy
+ * @Date: 2022-04-13 18:05:34
+ * @LastEditors: Roy
+ * @LastEditTime: 2022-08-18 15:51:48
+ * @Description: Graph class
+ */
+import G6 from '@antv/g6';
+import { IGraph } from './../interface/graph';
+import Util from './../util';
+import { IEventForCanvas } from './../../typings/custom';
+import { PluginBase, GraphOptions, INode } from './../../typings/graph';
 import Command from './../command/index';
-import { PluginBase } from '@antv/g6-plugin';
 
 /**
  * 注册util
@@ -19,7 +28,7 @@ export interface RegisterFunction {
 }
 
 @addUtilMethods(Util)
-class Graph extends G6.Graph {
+class Graph extends G6.Graph implements IGraph {
   public cmd: any;
   static registerNode: RegisterFunction = (nodeName, options, extendedNodeName) => {
     G6.registerNode(nodeName, options, extendedNodeName);
@@ -39,6 +48,7 @@ class Graph extends G6.Graph {
     super(cfg);
     this.cmd = new Command(this);
   }
+
   /**
    * 注册插件
    * @param plugin
@@ -67,7 +77,75 @@ class Graph extends G6.Graph {
    */
   public css(style: any) {
     const graphContainer = super.get('canvas').get('el');
-    (this as any).modifyCSS(graphContainer, style);
+    Util.modifyCSS(graphContainer, style);
+  }
+  /**
+   * getNodes getEdges getCombos
+   * @param this
+   * @returns Items
+   */
+  public getItems() {
+    const itemMap = this.get('itemMap');
+    const rst: any[] = [];
+    Util.each(itemMap, (item: { getType: () => any }) => {
+      if (item.getType()) {
+        rst.push(item);
+      }
+    });
+    return rst;
+  }
+
+  /**
+   * get item
+   * @param shape circle rect image text
+   * @returns Item
+   */
+  public getItem(shape: any) {
+    const itemMap = this.get('itemMap');
+    let item = null;
+    Util.each(itemMap, (el: { get: (arg0: string) => any }) => {
+      let group = el.get('group');
+      if (group.contain(shape)) {
+        item = el;
+      }
+    });
+    return item;
+  }
+
+  /**
+   * get item by shape
+   * @param shape
+   * @returns item
+   */
+  public getItemByShape(shape: any): INode | null {
+    if (!shape) return null;
+    return this!.getItem(shape);
+  }
+
+  /**
+   * 监听拖拽事件 获取canvas上对应坐标的元素
+   * @param this graph
+   * @param domEvent drop originalEvent
+   * @returns shape
+   */
+  public getEventForCanvas(domEvent: DragEvent): IEventForCanvas {
+    const clientX = domEvent.clientX;
+    const clientY = domEvent.clientY;
+    const canvas = this.get('canvas');
+    const point = this.getPointByClient(clientX, clientY);
+    const canvasPoint = this.getCanvasByPoint(point.x, point.y);
+    const shape = canvas.getShape(canvasPoint.x, canvasPoint.y, domEvent);
+    const item = this.getItemByShape(shape);
+    const pixelRatio = this.getZoom();
+    return {
+      item,
+      shape,
+      x: point.x,
+      y: point.y,
+      domX: canvasPoint.x / pixelRatio,
+      domY: canvasPoint.y / pixelRatio,
+      domEvent
+    };
   }
 }
 export default Graph;
