ce3ae980a84d9acc1a685877f0c2cadbfdf5e4e9
Roy <iRoySwift@iCloud.com>
2024-03-07 15:22:01 +0800

    theme

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,60 +1,14 @@
-// import { ThemeProvider } from '@emotion/react';
-import { styled } from '@mui/material/styles';
-// import * as locales from '@mui/material/locale';
-
 import Routers from '@/routes/index';
-// import { MAINNET_PRIMARY_THEME_COLOR, MAINNET_SECONDARY_THEME_COLOR } from './utils/const';
 import withProviders from './store/providers';
 import { useInitApp } from './hooks';
-import BrandingProvider from './components/Branding/BrandingProvider';
-
-// const Theme = {
-//   primary: MAINNET_PRIMARY_THEME_COLOR,
-//   secondary: MAINNET_SECONDARY_THEME_COLOR
-// };
-
-// const theme = createTheme(
-//   {
-//     palette: {
-//       primary: { main: MAINNET_PRIMARY_THEME_COLOR }
-//     }
-//   },
-//   zhCN
-// );
-const Container = styled('div')`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-`;
-// type SupportedLocales = keyof typeof locales;
+import ThemeCustomization from './theme';
 
 const App = withProviders(() => {
-  // const [locale, setLocale] = useState<SupportedLocales>('zhCN');
-
-  // const {
-  //   app: { mode }
-  // } = useAppState();
-
-  // const theme = useMemo(
-  //   () =>
-  //     createTheme({
-  //       palette: {
-  //         mode
-  //       }
-  //     }),
-  //   [mode]
-  // );
-
-  // const themeWithLocale = useMemo(() => createTheme(theme, locales['zhCN']), ['zhCN', theme]);
-
   useInitApp();
   return (
-    <BrandingProvider>
-      <Container>
-        <Routers />
-      </Container>
-    </BrandingProvider>
+    <ThemeCustomization>
+      <Routers />
+    </ThemeCustomization>
   );
 });
 
---
+++ example/src/theme/index.tsx
@@ -0,0 +1,43 @@
+import { createTheme, CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material';
+// ** Theme Override Imports
+import overrides from './overrides';
+
+// ** Theme
+import themeOptions from './ThemeOptions';
+import * as locales from '@mui/material/locale';
+// import { useLayoutState } from "@/content/withLayoutContent";
+import { useMemo } from 'react';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const ThemeCustomization: React.FC<Props> = ({ children }) => {
+  // const { language } = useLayoutState();
+
+  // ** Merged ThemeOptions of Core and User
+  const coreThemeConfig = themeOptions();
+  // const theme = useTheme();
+
+  const themeWithLocale = useMemo(() => {
+    // ** Pass ThemeOptions to CreateTheme Function to create partial theme without component overrides
+    let theme = createTheme(coreThemeConfig, locales['zhCN']);
+    // themes.components = componentsOverride(themes);
+    // ** Continue theme creation and pass merged component overrides to CreateTheme function
+    theme = createTheme(theme, {
+      components: { ...overrides(theme) }
+    });
+    return theme;
+  }, [coreThemeConfig]);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={themeWithLocale}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+};
+
+export default ThemeCustomization;
